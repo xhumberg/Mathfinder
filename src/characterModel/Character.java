4f82654ae55cfaa8d6299ef7a4cfb5a3b375eaaf
Xavier Humberg <u0893045@utah.edu>
2019-05-06 22:44:50 -0600

    Initial commit

---
+++ src/characterModel/Character.java
@@ -0,0 +1,6 @@
+package characterModel;
+
+public class Character {
+	String Name;
+	int Str;
+}
---
+++ src/characterModel/CharacterTests.java
@@ -0,0 +1,16 @@
+package characterModel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CharacterTests {
+
+	@Test
+	void defaultTest() {
+		fail("Not yet implemented");
+	}
+
+	
+	
+}
