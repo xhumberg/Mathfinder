1bd05fd2f2756d61b211ae3e9c007a8d6148f65d
ckivory <ckivory@gmail.com>
2019-05-06 23:22:02 -0600

    To be reverted

--- src/characterModel/CharacterTests.java
+++ src/characterModel/CharacterTests.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class CreationAndVariableSetting {
+class CharacterTests {
 
 	@Test
 	void basicCharacterCreation() {
